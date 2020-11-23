59fbf74a23488289dd57b231d0e533f6f8d243cb
Amr Emam <amr.emam998@gmail.com>
2020-11-23 22:33:21 +0200

    Bus Reservation system almost completed

--- lib/Screens/user_loading_screen.dart
+++ lib/Screens/user_loading_screen.dart
@@ -7,85 +7,56 @@ import 'package:bus_reservation_systerm/Models/user_request.dart';
 import 'package:bus_reservation_systerm/Provider/user_data_provider.dart';
 import 'package:bus_reservation_systerm/Screens/home_screen.dart';
 import 'package:bus_reservation_systerm/Widgets/loadingTextWidget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class UserLoadingScreen extends StatelessWidget {
-
-
   String branch = "";
-
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: RetrieveDataFromFireStore.getUserByID(),
-      builder: (BuildContext context, AsyncSnapshot userSnapshot) {
-        if (userSnapshot.connectionState == ConnectionState.done) {
-          branch = userSnapshot.data['branch'];
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Provider.of<UserDataProvider>(context, listen: false)
-                .setUser(UserModel.fromSnapShot(userSnapshot.data));
-          });
-         return loadRequset();
-
-        }
-        return LoadingTextWidget();
-      }
-        );
-      }
-
-
-  getLines(AsyncSnapshot value) {
-    List<Line> lines = List();
-    value.data.docs.forEach((element) {
-      Line line = Line.fromSnapshot(element);
-      print(line.to);
-      lines.add(line);
-    });
-    return lines;
-  }
-
-
-
-
-
-
-  LoadLines(String branch) {
-    return FutureBuilder(
-        future: RetrieveDataFromFireStore.getLinesByBranch(branch),
-        builder: (BuildContext context, AsyncSnapshot linesSnapShot) {
-          if (linesSnapShot.connectionState == ConnectionState.done) {
+        future: _fetchData(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if(snapshot.data.exists){
             WidgetsBinding.instance.addPostFrameCallback((_) {
               Provider.of<UserDataProvider>(context, listen: false)
-                  .updateLines(getLines(linesSnapShot));
+                  .setRequest(UserRequest.fromAsyncSnapshot(snapshot.data));
             });
+            }
             return HomeScreen();
           }
           return LoadingTextWidget();
         });
   }
-  FutureBuilder loadRequset(){
-    return FutureBuilder(
-      future: RetrieveDataFromFireStore.getUserRequestById(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if(!snapshot.data.exists)
-          {
-            print('error');
-            return LoadLines(branch);
-          }
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            Provider.of<UserDataProvider>(context, listen: false)
-                .setRequest(UserRequest.fromAsyncSnapshot(snapshot.data));
-          });
-          return LoadLines(branch);
-        }
-        return LoadingTextWidget();
-      },
 
-    );
+
+
+  getLinesFormSnapshot(QuerySnapshot value) {
+    List<Line> lines = List();
+    value.docs.forEach((element) {
+    lines.add(Line.fromSnapshot(element));
+    });
+    return lines;
+  }
+
+
+ Future<dynamic> _fetchData(BuildContext context) async {
+    DocumentSnapshot userDocument =
+        await RetrieveDataFromFireStore.getUserByID();
+    branch = userDocument.data()['branch'];
+    QuerySnapshot linesSnapshot =
+        await RetrieveDataFromFireStore.getLinesByBranch(branch);
+    getLinesFormSnapshot(linesSnapshot);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<UserDataProvider>(context, listen: false)
+          .updateLines(getLinesFormSnapshot(linesSnapshot));
+      Provider.of<UserDataProvider>(context, listen: false)
+          .setUser(UserModel.fromSnapShot(userDocument));
+
+    });
+    return RetrieveDataFromFireStore.getUserRequestById();
   }
 }
-
